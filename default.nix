f017eee616c1deebdf8cb6c7b2939d070f1edfcc
Lanny <lan.rogers.book@gmail.com>
2022-12-08 16:03:19 -0600

    Add nix file.

---
+++ default.nix
@@ -0,0 +1,17 @@
+with import <nixpkgs> {};
+let 
+  pypkgs = python-packages: with python-packages; [ pandocfilters ]; 
+  python = pkgs.python3.withPackages pypkgs;
+in pkgs.stdenv.mkDerivation rec {
+  name = "lanysport.net";
+  description = "Lanny's personal site or whatever";
+
+  buildInputs = [
+    pkgs.pandoc
+    python
+  ];
+
+  src = ./.;
+}
+
+
